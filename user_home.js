a3ca73fc1706d5a8668a28351970987914e9e76a
Rachel Myre <rmmyre@gmail.com>
2018-05-21 20:19:48 +0000

    added an inialize function for timeslot. Changed createdTimeslot function to rearrange timeslot order based on times entered

--- user_home.js
+++ user_home.js
@@ -1,8 +1,11 @@
 "use strict";
 
 (function($){
+	var current = null;
+
 	var epochTime = {
-		day: 86400,
+		DAY: 86400,
+		HOUR: 3600,
 		newDate: new Date(),
 		weekLength: 7,
 
@@ -39,7 +42,7 @@
 		},
 		
 		epochDates: function(){
-			return this.datesFromCurrentDay(this.weekLength, this.day);
+			return this.datesFromCurrentDay(this.weekLength, this.DAY);
 		},
 		
 		weekOfDateObjects: function(){
@@ -99,7 +102,6 @@
 			return btnArray;
 		},
 		
-		//creates a date hmtl prefix
 		date: function(){
 			return $(this.html(this.prefix(), "date"));
 		},
@@ -123,10 +125,6 @@
 		dayBlockNumber: 1,
 		timeslotUniqueID: 1,
 		currentDate: "",
-
-		logDayBlockArray: function(){
-			console.log(this.dayBlockArray);
-		},
 
 		weekdayFormat: function(day){
 			return day.toLocaleDateString('en-US', { weekday: 'long', timeZone: 'UTC' });
@@ -173,33 +171,91 @@
 
 		newTimeslot: function(){
 			var timeslot = new Timeslot(this.blockDate(), this.createTimeslotID());
-			console.log("new timeslot created");
-			timeslot.showBlockContent();
-			timeslot.addTimesToBlockHeader();
-			timeslot.addDetailsToBlock();
-			timeslot.createDeleteButton();
-			timeslot.hideBlockContent();
 			return timeslot;
 		},
 		
-		addTimeslotToArray: function(blockNumber){
-			this.dayBlockNumber = blockNumber;
-			var slot = this.newTimeslot();
-			var day = slot.dateOfBlockDay;
-			this.timeslotArray.push(slot);
-			return day	
+		initializeTimeslot: function(timeslot, array){
+			if(timeslot.timeRangeIsCorrect()){
+				console.log("new timeslot created");
+				timeslot.showBlockContent();
+				timeslot.addTimesToBlockHeader();
+				timeslot.addDetailsToBlock();
+				timeslot.createDeleteButton();
+				timeslot.hideBlockContent();
+				this.addTimeslotToArray(timeslot);
+				array.push(timeslot.dateOfBlockDay);
+			} else {
+				console.log("no timeslot created");
+			}
+		},
+		
+		addTimeslotToArray: function(timeslot){
+			this.timeslotArray.push(timeslot);	
+		},
+		
+		getTimeslotByDate: function(date, blockArray){
+			//for every timeslot in the timeslotArray
+				//if the date of the timeslot is equal to the date and can be found in the block array
+					//return timeslot
+				
+			var slotArray = this.timeslotArray;
+			
+			for (var i = 0; i < slotArray.length; i++){
+				var timeslot = slotArray[i];
+				
+				if(timeslot.dateOfBlockDay === date && blockArray.indexOf(date) > -1 ){
+					timeslot = timeslot;
+				} else {
+					console.log("there is no matching date in timeslotArray");
+				}
+			}
+			
+			return timeslot;
 		},
 		
 		createTimeslot: function(){
 			var date = this.blockDate();
-			
+			var slot = this.newTimeslot();
+
 			if(this.timeslotArray.length < 1){
-				this.block1Array.push(this.addTimeslotToArray(1));
+				slot.setBlockNumber(1);
+				this.initializeTimeslot(slot, this.block1Array);
 			} else if(this.timeslotArray.length >= 1){
-				if(this.block1Array.indexOf(date) === -1){
-					this.block1Array.push(this.addTimeslotToArray(1));
+				if(this.block1Array.indexOf(date) === -1 && this.block2Array.indexOf(date) === -1){
+					slot.setBlockNumber(1);
+					this.initializeTimeslot(slot, this.block1Array);
+				} else if (this.block1Array.indexOf(date) === -1 && this.block2Array.indexOf(date) != -1){
+					var oldTimeslot = this.getTimeslotByDate(date, this.block2Array);
+
+					if (this.time1IsEarlierThanTime2(slot.endTime, oldTimeslot.startTime) === true){
+						slot.setBlockNumber(1);
+						this.initializeTimeslot(slot, this.block1Array);
+					} else if (this.time1IsEarlierThanTime2(oldTimeslot.endTime, slot.startTime) === true){
+						oldTimeslot.setBlockNumber(1);
+						this.deleteDayBlockFromArray(2, date);
+						this.initializeTimeslot(oldTimeslot, this.block1Array);
+						slot.setBlockNumber(2);
+						this.initializeTimeslot(slot, this.block2Array);
+						console.log("new slot start and end times are " + slot.startTime + " " + slot.endTime);
+					} else {
+						console.log("timeslots overlap " + oldTimeslot.startTime + slot.endTime);
+					}
 				} else if (this.block1Array.indexOf(date) != -1 && this.block2Array.indexOf(date) === -1){
-					this.block2Array.push(this.addTimeslotToArray(2));
+					oldTimeslot = this.getTimeslotByDate(date, this.block1Array);
+
+					if (this.time1IsEarlierThanTime2(oldTimeslot.endTime, slot.startTime) === true){
+						slot.setBlockNumber(2);
+						this.initializeTimeslot(slot, this.block2Array);
+					} else if (this.time1IsEarlierThanTime2(slot.endTime, oldTimeslot.startTime) === true){
+						oldTimeslot.setBlockNumber(2);
+						this.deleteDayBlockFromArray(1, date);
+						this.initializeTimeslot(oldTimeslot, this.block2Array);
+						slot.setBlockNumber(1);
+						this.initializeTimeslot(slot, this.block1Array);
+						console.log("new slot start and end times are " + slot.startTime + " " + slot.endTime);
+					} else {
+						console.log("timeslots overlap " + slot.startTime + oldTimeslot.endTime);
+					}
 				} 
 			}
 			console.log(this.timeslotArray);
@@ -248,6 +304,59 @@
 			}
 			return exp;
 		},
+		
+		time1IsEarlierThanTime2: function(time1, time2){
+			var isTime1Lesser = true;
+			
+			if(time1 > time2){
+				isTime1Lesser = false;
+			} else if (time1 <= time2){
+				console.log("Start time entered by user is valid");
+			}
+			return isTime1Lesser;
+		},
+		
+		test: function(timeslot){
+			var array1 = this.block1Array;
+			var slot = timeslot;
+			slot.log();
+			
+			if(array1.indexOf(slot.dateOfBlockDay) === -1){
+				console.log("this date does not exist in block1");
+			} else {
+				var indexOfDate = array1.indexOf(slot.dateOfBlockDay);
+				var dateOfTimeslotInBlock1 = array1[indexOfDate];
+				
+				var startTime1 = this.getEndTime(dateOfTimeslotInBlock1);
+				var startTime2 = slot.startTime;
+				console.log("index of date is: " + dateOfTimeslotInBlock1);
+				console.log("Start time 1 is: " + startTime1);
+				console.log("Start time 2 is: " + startTime2);
+				
+				if(this.time1IsEarlierThanTime2(startTime1, startTime2)){
+					slot.blockNumber = 2;
+				} else {
+					console.log("Timeslot two is in the same range as block 1");
+				}
+			}
+			
+			console.log(slot.blockIDnumber);
+		},
+		
+		getEndTime: function(date){
+			var array = this.timeslotArray;
+			var time;
+			
+			for (var i = 0; i < array.length; i++){
+				var timeslot = array[i];
+				if (timeslot.dateOfBlockDay === date){
+					var time = timeslot.endTime;
+				}
+			}
+			
+			console.log(time);
+			return time;
+		},
 
 		displayCalendar: function(){
 			this.textToDisplay(this.formatArrayItems(epochTime.weekOfDateObjects(), this.dateFormat), blockID.dateArray());
@@ -258,20 +367,41 @@
 	function Timeslot(dateOfBlockDay, slotID){
 		this.dateOfBlockDay = dateOfBlockDay;
 		this.blockIDPrefix = blockID.prefix();
-		this.blockIDnumber = blockID.number(calendarDisplay.dayBlockNumber);
+		this.blockNumber = 0;
 		this.$timeslotStart = $("#timeslot-start");
 		this.$timeslotEnd = $("#timeslot-end");
 		this.deleteButton = "";
 		this.startTime = this.$timeslotStart.val();
 		this.endTime = this.$timeslotEnd.val();
-		this.blockID = blockID.html(this.blockIDPrefix, this.blockIDnumber);
 		this.deleted = false;
 		this.slotID = slotID;
+		
+		this.blockIDnumber = function(){
+			var idNumber = blockID.number(this.blockNumber);
+			console.log("blockIDnumber is: " + idNumber);
+			return idNumber;
+		};
+		
+		this.blockID = function(){
+			var id = blockID.html(this.blockIDPrefix, this.blockIDnumber());
+			return id;
+		};
+		
+		
+		this.log = function(){
+			console.log("working as expected");
+		};
+		
+		this.setBlockNumber = function(number){
+			this.blockNumber = number;
+			console.log("setBlockNumber has set block number to: " + this.blockNumber);
+			return this.blockNumber;
+		};
 
 		this.showBlockContent = function(){
-			var content = $(blockID.html(this.blockIDPrefix, this.blockIDnumber));
+			var content = $(blockID.html(this.blockIDPrefix, this.blockIDnumber()));
 			console.log("showing timeslot content");
-			console.log(blockID.html(this.blockIDPrefix, this.blockIDnumber));
+			console.log(blockID.html(this.blockIDPrefix, this.blockIDnumber()));
 			content.show();
 		};
 		
@@ -282,8 +412,49 @@
 		
 		//converts the start and end times of the meetup to text for display
 		this.addTimesToBlockHeader = function(){
-			blockID.header(calendarDisplay.dayBlockNumber).text(this.timeInputToString(this.startTime) + this.am_pm(this.startTime) + " to " + this.timeInputToString(this.endTime) + this.am_pm(this.endTime));
+			var startTime = this.startTime;
+			var endTime = this.endTime;
+			blockID.header(this.blockNumber).text(this.timeInputToString(startTime) + this.am_pm(startTime) + " to " + this.timeInputToString(endTime) + this.am_pm(endTime));
 			//console.log("Start time is " + this.startTime);
+		};
+		
+		this.timeRangeIsCorrect = function(){
+			var timesAreValid = false;
+			var time1 = this.createTimeObject(this.startTime);
+			var time2 = this.createTimeObject(this.endTime);	
+			var difference = Math.abs(time1 - time2);
+			var hours = (difference/epochTime.HOUR)/1000;
+			console.log("difference between times is " + hours);
+			
+			if(calendarDisplay.time1IsEarlierThanTime2(time1, time2)){
+				if(hours < 2 || hours > 24){
+					console.log("Time range is outside of matching range");
+				} else if(hours >= 2 && hours < 24){
+					console.log("Time range is within matching range");
+					timesAreValid = true;
+				} else if(isNaN(hours)){
+					console.log("difference between start and end times is not a valid number");
+				}
+			} else {
+				console.log("Invalid time range entered by user");
+			}
+			
+			return timesAreValid;
+		};
+		
+		this.time1IsEarlierThanTime2 = function(time1, time2){
+			var isTime1Lesser = true;
+			
+			if(time1 >= time2){
+				isTime1Lesser = false;
+			} else if (time1 < time2){
+				console.log("Start time entered by user is valid");
+			}
+			return isTime1Lesser;
+		};
+		
+		this.putTimeslotsInOrder = function(){
+			
 		};
 		
 		//chooses "am" or "pm" for display on calendar based the on user time input
@@ -308,6 +479,23 @@
 		this.timeInputMinutes = function(time){
 			var exp = calendarDisplay.timeInputRegEx(time);
 			return exp[2];
+		};
+		
+		this.createTimeObject = function(time){
+			var initialDate = epochTime.createInitialDay();
+			var interval = calendarDisplay.buttonPressed * epochTime.DAY;
+			var epochDate = initialDate + interval;
+			var newDate = new Date(epochDate * 1000);
+			var hours = this.timeInputHours(time);
+			var minutes = this.timeInputMinutes(time);
+			newDate.setHours(hours, minutes);
+			
+			console.log(initialDate);
+			console.log(interval);
+			console.log(epochDate);
+			console.log(newDate.toLocaleString());
+			
+			return newDate;
 		};
 		
 		//creates a new, formatted string from the user time input, for display
@@ -340,23 +528,24 @@
 		
 		//converts block details of meetup into text for display
 		this.addDetailsToBlock = function(){
-			blockID.details(calendarDisplay.dayBlockNumber).text(this.blockDetails);
+			blockID.details(this.blockNumber).text(this.blockDetails);
 		};
 		
 		//creates a delete button for the content block
 		this.createDeleteButton = function(){
-			var btn = this.blockID + "-delete";
+			var btn = this.blockID() + "-delete";
 			this.deleteButton = btn;
 			console.log(this.deleteButton);
 		};
 		
 		//hides the content block on click
 		this.hideBlockContent = function(){
-			var content = $(this.blockID);
-			var number = calendarDisplay.dayBlockNumber;
+			var content = $(this.blockID());
+			var number = this.blockNumber;
 			var day = this.dateOfBlockDay;
 			var id = this.slotID;
-			console.log("hidden is " + this.blockID);
+			console.log("hideBlockContent blockID is " + this.blockID());
+			console.log("hideBlockContent dateOfBlockDay is " + this.dateOfBlockDay);
 			$(this.deleteButton).click(function(){
 				content.hide();
 				calendarDisplay.deleteDayBlockFromArray(number, day);
@@ -374,6 +563,7 @@
 		var $submitButton = $("#time-submit");
 		var $modal = $("#myModal");
 		var $closeButton = $("#closeButton");
+		var $dateBlockWrap = $(".dateblock-wrap");
 		
 		//opens the modal and logs which "add" button was pushed, for use in adding blocks to the correct days
 		var showModal = function(btn, n){
@@ -385,8 +575,8 @@
 			});
 		};
 		
-		//close the modal when user hits the close button
 		(function(){
+		//close the modal when user hits the close button
 			$closeButton.click(function(){
 				$modal.hide();
 			});
